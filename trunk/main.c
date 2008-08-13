1ecf178819e3d4a31bd632610c5f709a32e38650
marius <marius@da981f62-ed57-4248-99b7-598fd1fc20d8>
2008-08-13 10:46:39 +0000

    renderer redesigned

--- trunk/main.c
+++ trunk/main.c
@@ -1,49 +1,81 @@
+/*
+ * Memphis - Cairo Rederer for OSM in C
+ * Copyright (C) 2008  <marius.rieder@durchmesser.ch>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 
-#include "ll.h"
-#include "main.h"
 #include "osm05.h"
-#include "libosm.h"
-#include "config02.h"
+#include "renderer.h"
+#include "ruleset.h"
+#include "strlist.h"
+
+// Global Vars
+int         debug;
+strList     *keyStrings;
+strList     *valStrings;
+
+void banner() {
+	fprintf(stdout,"Memphis OSM Renderer\n");
+}
+
+void usage(char *prog) {
+    banner();
+	fprintf(stdout,"%s [-v] <configfile> <datafile>\n", prog);
+}
 
 int main(int argc, char **argv) {
-    debug = 0;
-    tiled = 0;
+    debug = 1;
     char *cfgfn = NULL;
     char *osmfn = NULL;
-    
+   
     int i;
     for (i = 1; i < argc ; i++) {
-	   if ((!strcmp(argv[i], "-v")) || (!strcmp(argv[i], "--verbose"))) {
-	       debug++;
-	   } else if ((!strcmp(argv[i], "-t")) || (!strcmp(argv[i], "--tile"))) {
-	       tiled ++;
-	   } else if (cfgfn == NULL) {
-	       cfgfn = malloc(strlen(argv[i])+1);
-	       strncpy(cfgfn,argv[i], strlen((argv[i]))+1);
-	   } else if (osmfn == NULL) {
-	       osmfn = malloc(strlen(argv[i])+1);
-	       strncpy(osmfn,argv[i], strlen(argv[i])+1);
-	   }
-	}
-                    	
-	fprintf(stdout,"Memphis OSM Renderer\n");
-	
-	fprintf(stdout," Read Config\n");
-	cfgLayer *config;
-	config = (cfgLayer *) cfgRead(cfgfn);
-
-	fprintf(stdout," Read Source\n");
-	osmFile *osmSrc;
-	osmSrc = (osmFile *) osmRead(osmfn);
-
-	fprintf(stdout," Cairo Draw Tiles\n");
-    drawCairo(config, osmSrc);
+        if ((!strcmp(argv[i], "-q")) || (!strcmp(argv[i], "--quiet"))) {
+            debug--;
+        } else if ((!strcmp(argv[i], "-v")) || (!strcmp(argv[i], "--verbose"))) {
+            debug++;
+        } else if (cfgfn == NULL) {
+            cfgfn = argv[i];
+        } else if (osmfn == NULL) {
+            osmfn = argv[i];
+        } else {
+            usage((char *) *argv);
+            exit(-1);
+        }
+    }
     
-    cfgFree(config);
+    if (argc < 2 || cfgfn == NULL || osmfn == NULL) {
+        usage((char *) *argv);
+        exit(-1);
+    }
+                    	
+	banner();
+	
+	cfgRules *ruleset;
+	ruleset = (cfgRules *) rulesetRead(cfgfn);
+
+	osmFile *osm;
+	osm = (osmFile *) osmRead(osmfn);
+	
+	renderCairo(ruleset, osm);
     
     return(0);
 }
--- trunk/renderer.c
+++ trunk/renderer.c
@@ -1,126 +1,269 @@
-#include "libxml/parser.h"
-#include "stdio.h"
+/*
+ * Memphis - Cairo Rederer for OSM in C
+ * Copyright (C) 2008  <marius.rieder@durchmesser.ch>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#include <math.h>
+#include <time.h>
+#include <cairo.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "osm05.h"
+#include "libmercator.h"
+#include "list.h"
+#include "renderer.h"
+#include "ruleset.h"
+#include "strlist.h"
 
-char string1[]="user";
-char string2[]="timestamp";
+// External Vars
+extern int      debug;
+extern strList  *keyStrings;
+extern strList  *valStrings;
 
-/**
- * startDocumentDebug:
- * @ctxt:  An XML parser context
+/*
+ * function: drawPath
+ * @cr  Array of cairo resources
+ * @nd  Liked list if osmNd's
  *
- * called when the document start being processed.
+ * This function is used to prepare a Path.
  */
-static void
-startDocumentDebug(void *ctx )
-{
-    fprintf(stdout, "SAX.startDocument()\n");
+void drawPath(renderInfo *info, osmNd *nd) {
+    coordinates xy;
+    int i;
+    
+    for (i=0;i<=LAYERMAX-LAYERMIN;i++) {
+        xy = coord2xy(nd->node->lat,nd->node->lon,i+LAYERMIN);
+        cairo_move_to(info->cr[i],
+                        xy.x-info->offset[i].x,
+                        xy.y-info->offset[i].y);
+    }
+    nd = nd->next;
+    while(nd) {
+        for (i=0;i<=LAYERMAX-LAYERMIN;i++) {
+            xy = coord2xy(nd->node->lat,nd->node->lon,i+LAYERMIN);
+            cairo_line_to(info->cr[i],
+                            xy.x-info->offset[i].x,
+                            xy.y-info->offset[i].y);
+        }
+        nd = nd->next;
+    }
 }
 
-/**
- * endDocumentDebug:
- * @ctxt:  An XML parser context
+/*
+ * function: strokePath
+ * @cr  Array of cairo resources
  *
- * called when the document end has been detected.
+ * This function is stroke all current path without drawing anithing.
  */
-static void
-endDocumentDebug(void *ctx)
-{
-    fprintf(stdout, "SAX.endDocument()\n");
-}/*
- * SAX2 specific callbacks
- */
-/**
- * startElementNsDebug:
- * @ctxt:  An XML parser context
- * @name:  The element name
+void strokePath(renderInfo *info) {
+    int i;
+    
+    for (i=0;i<=LAYERMAX-LAYERMIN;i++) {
+        cairo_set_line_width (info->cr[i], 0);
+       cairo_stroke(info->cr[i]);
+    }
+}
+
+/*
+ * function: drawPolygone
  *
- * called when an opening tag has been processed.
+ * This function fill the prepared paths with the configured color.
  */
-static void
-startElementDebug(void *ctx,
-                    const xmlChar *localname,
-		    const xmlChar **attributes)
-{
-    fprintf(stdout, "SAX.startElement(%s", (char *) localname);
-    while (*attributes != NULL) {
-        if(strncmp((char *) *(attributes), string1, 4) == 0) {
-            attributes+=2;
-            continue;}
-        if(strncmp((char *) *(attributes), string2, 9) == 0) {
-            attributes+=2;
-            continue;}
-		fprintf(stdout, ", %s:", (char *) *(attributes++));
-		fprintf(stdout, "%s", (char *) *(attributes++));
+void drawPolygone(renderInfo *info, cfgDraw *draw) {
+    if (debug > 1)
+        fprintf(stdout,"drawPolygone\n");
+    
+    int i;
+    for (i=0;i<=LAYERMAX-LAYERMIN;i++) {
+        cairo_set_fill_rule (info->cr[i], CAIRO_FILL_RULE_EVEN_ODD);
+        cairo_set_source_rgb (info->cr[i], draw->color[0],draw->color[1],
+                                        draw->color[2]);
+        cairo_fill_preserve(info->cr[i]);
+    }
+}
+
+/*
+ * function: drawLine
+ *
+ * This function draw the prepared paths with the configured color.
+ */
+void drawLine(renderInfo *info, cfgDraw *draw) {
+    if (debug > 1)
+        fprintf(stdout,"drawLine\n");
+    
+    int i;
+    for (i=0;i<=LAYERMAX-LAYERMIN;i++) {
+        cairo_set_line_cap  (info->cr[i], CAIRO_LINE_CAP_ROUND);
+        cairo_set_line_width (info->cr[i], draw->width*LINESIZE(i));
+
+        cairo_set_source_rgb (info->cr[i], draw->color[0],draw->color[1],
+                                        draw->color[2]);
+        cairo_stroke_preserve(info->cr[i]);
+    }
+}
+
+int stringInStrings(char *string, char **strings) {
+    if (debug > 1)
+        fprintf(stdout,"stringInStrings\n");
+    int r=0;
+    while (*strings != NULL) {
+        if (string == *strings)
+            return(1);
+        if(strcmp(*strings,"*") == 0)
+            r = 2;
+        if(strcmp(*strings,"~") == 0)
+            r = 3;
+        
+        *strings++;
+    }
+    return(r);
+}
+
+int matchRule(cfgRule *rule, osmTag *tag) {
+    if (debug > 1)
+        fprintf(stdout,"matchRule\n");
+    int k, v;
+    while(tag) {
+        k = 0;
+        k = stringInStrings(tag->key, rule->key);
+        v = 0;
+        v = stringInStrings(tag->value, rule->value);
+                
+        if (k==1 && v==1)
+            return(1);
+        if (k==1 && v==2)
+            return(1);
+        if (k==0 && v==3)
+            return(1);
+        
+        tag = tag->next;
+    }
+    return(0);
+}
+
+cfgDraw* applyWayRule(cfgRule *rule, osmWay *way) {
+    if (debug > 1)
+        fprintf(stdout,"applyWayRule\n");
+    cfgRule *end = rule->next;
+    while(rule) {
+        if(rule == end)
+            break;
+        if(matchRule(rule, way->tag)) {
+            
+            if (rule->sub != NULL)
+                rule = rule->sub;
+            else
+                return rule->draw;
+        } else {
+            if (rule->nsub != NULL) {
+                if (rule->nsub->sub != NULL)
+                    rule = rule->nsub->sub;
+                else
+                    return rule->nsub->draw;
+            } else {
+                rule = rule->next;
+            }
+        }
+        
 	}
-    fprintf(stdout, ")\n");
-}
-
-/**
- * endElementDebug:
- * @ctxt:  An XML parser context
- * @name:  The element name
- *
- * called when the end of an element has been detected.
- */
-static void
-endElementDebug(void *ctx ,
-                  const xmlChar *localname)
-{
-    fprintf(stdout, "SAX.endElement(%s)\n", (char *) localname);
-}
-
-
-xmlSAXHandler debugSAX2HandlerStruct = {
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    startDocumentDebug,
-    endDocumentDebug,
-    startElementDebug,
-    endElementDebug,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL
+	return(NULL);
 };
 
-xmlSAXHandlerPtr debugSAX2Handler = &debugSAX2HandlerStruct;
+int renderCairo(cfgRules *ruleset, osmFile *osm) {
+    if (debug > 1)
+        fprintf(stdout,"renderCairo\n");
+    int z, l;
+    renderInfo *info;
+    
+    info = malloc(sizeof(renderInfo));
+    
+    // Calculate X/Y Draw offsets
+    for (z=0;z<=LAYERMAX-LAYERMIN;z++) {
+        info->offset[z] = coord2xy(osm->bounds->maxlat,
+                                    osm->bounds->minlon,z+LAYERMIN);
+                                    
+        info->surface[z] = cairo_image_surface_create(CAIRO_FORMAT_ARGB32,
+                    TILESIZE*numTiles(z), TILESIZE* numTiles(z));
+        info->cr[z] = cairo_create(info->surface[z]);
+   }
+    
+    // Vars uder while looping throug data
+    osmWay      *way;
+    //osmTag      *tag;
+    cfgRule     *rule;
+    cfgDraw     *draw;
+    
+    long start = (long)clock();
+    
+    // Process Rule by Rule
+    LIST_FOREACH(rule, ruleset->rule) {
+        if (debug > 1)
+            fprintf(stdout,"rule\n");
+        
+        // Start checking osm from bottom layer.
+        for(l=-5;l<=5;l++) {
+            // Loop throug osmdata
+            LIST_FOREACH(way, osm->ways) {
+                //Only objects on current layer
+                if(way->layer != l)
+                    continue;
+                draw = applyWayRule(rule,way);
+                if(draw) {
+                    drawPath(info, way->nd);
+                    while(draw) {
+                        switch(draw->type) {
+                            case POLYGONE:
+                                drawPolygone(info,draw);
+                                break;
+                            case LINE:
+                                drawLine(info,draw);
+                                break;
+                        }
+                        draw = draw->next;
+                    }
+                    strokePath(info);
+                }
+            }
+        }
+    }
+    
+    
+    if (debug > 0)
+        fprintf(stdout," Cairo draw done.  [%fs]\n",
+                ((long)clock()-start)/(double)CLOCKS_PER_SEC); 
+    
+    // Saving Images
+    char *filename;
+    filename = malloc(sizeof(char)*50);
+    
+    for (z=0;z<=LAYERMAX-LAYERMIN;z++) {
+        start = (long)clock();
+        sprintf(filename,"tiles/%02i.png",z+LAYERMIN);
+        cairo_surface_write_to_png(info->surface[z], filename);
+        cairo_destroy(info->cr[z]);
+        cairo_surface_destroy(info->surface[z]);
+        if (debug > 0)
+            fprintf(stdout,"  Cairo rendering Z%i done.  [%fs]\n", z+LAYERMIN,
+                    ((long)clock()-start)/(double)CLOCKS_PER_SEC); 
+   }
 
-int main(int argc, char **argv) {
     
-    xmlInitParser();
-    
-    xmlParserCtxtPtr ctxt;
-    int res;
-    
-    res = xmlSAXUserParseFile(debugSAX2Handler, NULL,
-			                          "map.osm");
-    
-    xmlCleanupParser();
-    xmlMemoryDump();
 
     return(0);
 }
