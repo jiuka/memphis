dc2db7d813d58b2f7b39c8e593dc7c91cc52f0a1
Marius Rieder <marius.rieder@durchmesser.ch>
2008-08-15 11:24:14 +0000

    added tiles and fixed file style

--- compat.c
+++ compat.c
@@ -1,80 +1,48 @@
-#ifndef HAVE_STRSEP
-
 /*
- * strsep() implementation for systems that don't have it.
+ * Memphis - Cairo Rederer for OSM in C
+ * Copyright (C) 2008  <marius.rieder@durchmesser.ch>
  *
- * Copyright (c) 1990, 1993
- *	The Regents of the University of California.  All rights reserved.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc.,
  */
 
+#ifndef HAVE_STRSEP
 
 #include "stdio.h"
 
-/* 
-#if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)strsep.c	8.1 (Berkeley) 6/4/93";
-#endif */  /* LIBC_SCCS and not lint */
+char *strsep(char **stringp, const char *delim) {
+    register char *s;
+    register const char *spanp;
+    register int c, sc;
+    char *tok;
 
-
-/*
- * Get next token from string *stringp, where tokens are possibly-empty
- * strings separated by characters from delim.
- *
- * Writes NULs into the string at *stringp to end tokens.
- * delim need not remain constant from call to call.
- * On return, *stringp points past the last NUL written (if there might
- * be further tokens), or is NULL (if there are definitely no more tokens).
- *
- * If *stringp is NULL, strsep returns NULL.
- */
-
-char *strsep(char **stringp, const char *delim)
-{
-   register char *s;
-   register const char *spanp;
-   register int c, sc;
-   char *tok;
-   
-   if ((s = *stringp) == NULL)
-      return (NULL);
-   for (tok = s;;) {
-      c = *s++;
-      spanp = delim;
-      do {
-	 if ((sc = *spanp++) == c) {
-	    if (c == 0)
-	       s = NULL;
-	    else
-	       s[-1] = 0;
-	    *stringp = s;
-	    return (tok);
-	 }
-      } while (sc != 0);
-   }
+    if ((s = *stringp) == NULL)
+        return (NULL);
+    for (tok = s;;) {
+        c = *s++;
+        spanp = delim;
+        do {
+            if ((sc = *spanp++) == c) {
+                if (c == 0)
+                    s = NULL;
+                else
+                    s[-1] = 0;
+                *stringp = s;
+                return (tok);
+            }
+        } while (sc != 0);
+    }
 }
 
 #endif
@@ -84,17 +52,20 @@ char *strsep(char **stringp, const char *delim)
 #include <string.h>
 #include <stdlib.h>
 
-char *strdup(const char *src)
-{
-      size_t n = strlen(src);
-      char *dst = malloc(n+1);
+char *strdup(const char *src) {
+    size_t n = strlen(src);
+    char *dst = malloc(n+1);
 
-	 if (!dst)
-		  return NULL;
+    if (!dst)
+        return NULL;
 
-	    dst[n] = '\0';
-	       return (char *)memcpy(dst, src, n);
+    dst[n] = '\0';
+
+    return (char *)memcpy(dst, src, n);
 }
+
 #endif
 
-
+/*
+ * vim: expandtab shiftwidth=4 tabstop=4:
+ */
